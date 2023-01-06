aadb9feaad437fe79d21676943bc2ef376636092
Zain Hassan <zainhassan927@gmail.com>
2023-01-06 21:49:02 +0500

    add the controls

--- inc/forms.php
+++ inc/forms.php
@@ -19,18 +19,6 @@
  */
 class forms_widget_custom_elementor  extends \Elementor\Widget_Base {
 	
-
-	public function get_style_depends() {
-
-		//wp_register_style( 'dropDown-style', plugins_url( 'assets/css/dropdown.css', __FILE__ ) );
-
-		return [
-			//'dropDown-style',
-		];
-
-	}
-	
-
 	/**
 	 * Get widget name.
 	 *
@@ -127,10 +115,184 @@ class forms_widget_custom_elementor  extends \Elementor\Widget_Base {
 				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 		);
-	
 
+		$this->add_control(
+			'widget_title',
+			[
+				'label' => esc_html__( 'Title', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Default title', 'redirect-forms' ),
+				'placeholder' => esc_html__( 'Type your title here', 'redirect-forms' ),
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'widget_subtitle',
+			[
+				'label' => esc_html__( 'Sub Title', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Default sub title', 'redirect-forms' ),
+				'placeholder' => esc_html__( 'Type your title here', 'redirect-forms' ),
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'show_options',
+			[
+				'label' => esc_html__( 'Show Select Options', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::SWITCHER,
+				'label_on' => esc_html__( 'Show', 'redirect-forms' ),
+				'label_off' => esc_html__( 'Hide', 'redirect-forms' ),
+				'return_value' => 'yes',
+				'default' => 'yes',
+			]
+		);
+
+		$this->add_control(
+			'options_list',
+			[
+				'label' => esc_html__( 'Add Options', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => [
+					[
+						'name' => 'list_title',
+						'label' => esc_html__( 'Option', 'redirect-forms' ),
+						'type' => \Elementor\Controls_Manager::TEXT,
+						'default' => esc_html__( 'List Title' , 'redirect-forms' ),
+						'label_block' => true,
+					],
+					[
+						'name' => 'redirect_link',
+						'label' => esc_html__( 'Redirect Link', 'redirect-forms' ),
+						'type' => \Elementor\Controls_Manager::URL,
+						'placeholder' => esc_html__( 'https://your-link.com', 'redirect-forms' ),
+						'options' => [ 'url', 'is_external', 'nofollow' ],
+						'default' => [
+							'url' => '',
+							'is_external' => true,
+							'nofollow' => true,
+							// 'custom_attributes' => '',
+						],
+						'label_block' => true,
+					]
+				],
+				'default' => [
+					[
+						'list_title' => esc_html__( 'Option #1', 'redirect-forms' ),
+						'redirect_link' => esc_html__( 'https://your-link.com', 'redirect-forms' ),
+					],
+				],
+				'title_field' => '{{{ list_title }}}',
+				'condition' => [
+					'show_options' => 'yes',
+				],
+			]
+		);
+
+		$this->add_control(
+			'redirect_simple_link',
+			[
+				'label' => esc_html__( 'Redirect Link', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::URL,
+				'placeholder' => esc_html__( 'https://your-link.com', 'redirect-forms' ),
+				'options' => [ 'url', 'is_external', 'nofollow' ],
+				'default' => [
+					'url' => '',
+					'is_external' => true,
+					'nofollow' => true,
+					// 'custom_attributes' => '',
+				],
+				'label_block' => true,
+				'condition' => [
+					'show_options!' => 'yes',
+				],
+			]
+		);
+
+		$this->add_control(
+			'widget_button',
+			[
+				'label' => esc_html__( 'Button Text', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'default' => esc_html__( 'Get Quote', 'redirect-forms' ),
+				'placeholder' => esc_html__( 'Type your title here', 'redirect-forms' ),
+				'label_block' => true,
+			]
+		);
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => esc_html__( 'Style', 'redirect-forms' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'content_typography1',
+				'label' => esc_html__( 'Title Typography', 'redirect-forms' ),
+				'selector' => '{{WRAPPER}} h2',
+			]
+		);		
+		
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'content_typography2',
+				'label' => esc_html__( 'Sub Title Typography', 'redirect-forms' ),
+				'selector' => '{{WRAPPER}} p',
+			]
+		);
+		
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'content_typography3',
+				'label' => esc_html__( 'Button Typography', 'redirect-forms' ),
+				'selector' => '{{WRAPPER}} button.getQuoteBtn',
+			]
+		);
+
+		$this->add_control(
+			'button_color',
+			[
+				'label' => esc_html__( 'Button Color', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} button.getQuoteBtn' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'header_color',
+			[
+				'label' => esc_html__( 'Header Background Color', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .quote-form-title' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'body_color',
+			[
+				'label' => esc_html__( 'Body Background Color', 'redirect-forms' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .quote-form-body' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
 
 		$this->end_controls_section();
+
 
 
 	}
@@ -145,32 +307,17 @@ class forms_widget_custom_elementor  extends \Elementor\Widget_Base {
 	 */
 	protected function render() {
 		$settings = $this->get_settings_for_display();
+		$redirect_link = 'yes';
+
+		$length = 5;
+		$randomletter = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz"), 0, $length);
+		
 
 
 		
         ?>
-		<style id="wp-custom-css">
-			.page-id-6377 p {
-                margin-bottom: 20px;
-            }
-            .row.ser-1 {
-                width: 100%;
-                float: left;
-                margin-bottom:40px;
-            }
-            .ser-1 .col-sm-6 {
-                float: left;
-                width: calc(50% - 10px);
-                margin: 5px;
-            }
-            .ast-single-post .entry-title, .page-title {
-                font-weight: 900;
-            }
-            .elementor-widget.elementor-list-item-link-full_width a {
-                width: 100%;
-                text-decoration: none;
-            }
-
+		<style>
+			
             .quote-form {
                 max-width: 300px;
                 width: 100%;
@@ -208,47 +355,61 @@ class forms_widget_custom_elementor  extends \Elementor\Widget_Base {
                 border-bottom-right-radius: 4px;
             }
 
-            .box-desc-left .elementor-image-box-description{
-                text-align:left
-            }
-            @media screen and (min-width:320px) and (max-width:767px){
-                .ser-1 .col-sm-6 {
-                    width: calc(100% - 10px);
-                }
-                .ser-map iframe {
-                    width: 100%;
-                }
-            }		
+			.quote-form button{
+				border: 0;
+				outline: none;
+			}
+		
         </style>
-        <form class="quote-form rounded large">
+        <form class="quote-form rounded large <?php echo $randomletter; ?>">
             <div class="quote-form-title">
-                <h2>Health Dental Vision Insurance Quote</h2>
+                <h2><?php echo $settings['widget_title']; ?></h2>
             </div>
             <div class="quote-form-body">
-                <p>Compare Health Insurance Online Now</p>
+                <p><?php echo $settings['widget_subtitle']; ?></p>
                 <div class="form-group" style="margin-bottom: 15px">
                 <input type="number" class="form-control" name="zipcode" placeholder="Enter your zip code">
                 <div data-lastpass-icon-root="true" style="position: relative !important; height: 0px !important; width: 0px !important; float: left !important;"></div></div>
-                <div class="form-group" style="margin-bottom: 15px">
-                    <select name="redirectUrl" class="form-control">
-                        <option value="https://www.healthsherpa.com/?_agent_id=blake-nwosu">Health Insurance</option>
-                        <option value="https://www.uhone.com/shop/#/census?brokerid=AA4509053">Short-Term Medical</option>
-                        <option value="https://www.uhone.com/shop/#/census?brokerid=AA4509053">Vision</option>
-                        <option value="https://www.uhone.com/shop/#/census?brokerid=AA4509053">Dental</option>
-                    </select>
-                </div>
-                <button type="button" class="btn btn-primary getQuoteBtn">Get Quote</button>
+				<?php
+				if($settings['show_options'] === 'yes'){
+					?>
+					<div class="form-group" style="margin-bottom: 15px">
+						<select name="redirectUrl" class="form-control">
+							<?php
+							if ( $settings['options_list'] ) {
+								foreach($settings['options_list'] as $item){
+									?>
+									<option value="<?php echo $item['redirect_link']['url'] ?>"><?php echo $item['list_title'] ?></option>
+									<?php
+								}
+
+							}
+							?>
+						</select>
+					</div>
+					<?php
+				}else{
+					$redirect_link = $settings['redirect_simple_link']['url'];
+				}
+				?>
+
+                <button type="button" class="btn btn-primary getQuoteBtn"><?php echo $settings['widget_button']; ?></button>
 
                 <script type="text/javascript">
-                    jQuery('.getQuoteBtn').on('click', function(e){
+                    jQuery('.<?php echo $randomletter; ?> .getQuoteBtn').on('click', function(e){
                         const $form = jQuery(this).parents('.quote-form-body');
                         const redirectUrl = $form.find('[name=redirectUrl]').val();
                         const zipCode = $form.find('[name=zipcode]').val();
-                        if(zipCode) {
-                            window.location.href = `${redirectUrl}&zip_code=${zipCode}`;
-                        } else {
-                            window.location.href = `${redirectUrl}`;
-                        }
+						if('yes' == '<?php echo $redirect_link; ?>'){
+							if(zipCode) {
+								// window.location.href = `${redirectUrl}&zip_code=${zipCode}`;
+								window.location.href = `${redirectUrl}`;
+							} else {
+								window.location.href = `${redirectUrl}`;
+							}
+						}else{
+							window.location.href = '<?php echo $redirect_link; ?>';
+						}
                         e.preventDefault();
                     });
                 </script>
